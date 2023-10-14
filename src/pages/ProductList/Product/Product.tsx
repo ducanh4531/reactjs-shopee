568251fae2b5a870a62ff44168801360cbf4755d
ducanh4531 <ducanhle1794@gmail.com>
2023-10-14 15:27:49 +0800

    add Product component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,5 +1,75 @@
+import { Link } from 'react-router-dom'
+import pagePath from 'src/constants/path'
+
 const Product = () => {
-  return <div>Product</div>
+  return (
+    <Link to={pagePath.home}>
+      <div className='bg-white shadow overflow-hidden rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform'>
+        <div className='relative w-full pt-[100%]'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/c1adc9a5e622bf3f645a7e2ac446cd13_tn'
+            alt='product'
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>
+            Chuột máy tính Gaming Latope chuột máy tính có dây Full Led chơi game K2
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-through max-w-[50%] text-gray-500 truncate'>
+              <span className='text-xs'>₫</span>
+              <span>100.000</span>
+            </div>
+            <div className='text-orange truncate ml-1'>
+              <span className='text-xs'>₫</span>
+              <span>80.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden w-[50%]'>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='h-3 w-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='h-3 w-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>5.66k</span>
+              <span className='ml-1'>sold</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
 
 export default Product
